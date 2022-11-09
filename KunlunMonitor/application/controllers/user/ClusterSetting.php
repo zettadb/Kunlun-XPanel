6dfaffa0f09a36aa17895dbae0fefb67ae9345af
wangwentao <taogov@gmail.com>
2022-11-09 12:25:44 +0800

    获取PG的表列表接口

--- KunlunMonitor/application/controllers/user/ClusterSetting.php
+++ KunlunMonitor/application/controllers/user/ClusterSetting.php
@@ -13,6 +13,81 @@ class ClusterSetting extends CI_Controller
 		$this->post_url = $this->config->item('post_url');
 	}
 
+	/**
+	 * 获取PG的表列表
+	 * @throws
+	 * @author wangwentao
+	 */
+	public function getPGTableList()
+	{
+		$username = $this->input->get('name');
+		$clusterId = $this->input->get('cluster_id');
+
+		$sql = "select id,name from db_clusters where memo!='' and memo is not null and status!='deleted' and id = '{$clusterId}'";
+		if (!empty($username)) {
+			$sql .= " and nick_name like '%$username%'";
+		}
+		$this->load->model('Cluster_model');
+		$res = $this->Cluster_model->getList($sql);
+		if ($res === false) {
+			throw new ApiException('所选集群不存在');
+		}
+		$nodeSql = "select hostaddr,port,user_name,passwd from comp_nodes where db_cluster_id = {$clusterId} and status <> 'deleted' order by id asc limit 1";
+		$res = $this->Cluster_model->getList($nodeSql);
+		if ($res === false) {
+			throw new ApiException('未查询到PG节点');
+		}
+		$node = current($res);
+		$con = get_pg_con($node['hostaddr'], $node['port'], $node['user_name'], $node['passwd'], 'postgres');
+		// 查询所有库
+		$databases = pg_find($con, 'SELECT datname FROM pg_database WHERE datistemplate = false');
+		if (empty($databases)) {
+			throw new ApiException('PG数据库为空');
+		}
+		// 遍历查询所有表信息
+		$tableOwners = implode("','", array_column($databases, 'datname'));
+		$tableOwners = "'" . $tableOwners . "'";
+		$sql = "select schemaname as schema,tablename as table,tableowner as db from pg_tables where tableowner in ({$tableOwners})";
+		$tables = pg_find($con, $sql);
+		$tableNodes = [];
+		foreach ($tables as $table) {
+			$tableNodes[$table['db']][$table['schema']][$table['table']] = $table['table'];
+		}
+		$data = [];
+		foreach ($tableNodes as $key => $tableNode) {
+			$list = [
+				'value' => $key,
+				'label' => $key,
+				'desc' => 'db',
+			];
+			$children = [];
+			foreach ($tableNode as $sk => $schema) {
+				$schemaList = [
+					'value' => $sk,
+					'label' => $sk,
+					'desc' => 'schema',
+				];
+				$schemaChildren = [];
+				foreach ($schema as $tKey => $tableName) {
+					$schemaChildren[] = [
+						'value' => $tKey,
+						'label' => $tKey,
+						'desc' => 'table',
+					];
+				}
+				$schemaList['children'] = $schemaChildren;
+				$children[] = $schemaList;
+			}
+			$list['children'] = $children;
+			$data[] = $list;
+		}
+
+		echo json_encode([
+			'code' => 200,
+			'list' => $data,
+		]);
+	}
+
 
 	/**
 	 * 集群下拉选项
@@ -20,7 +95,26 @@ class ClusterSetting extends CI_Controller
 	 */
 	public function clusterOptions()
 	{
-
+		$username = $this->input->get('name');
+		$filterId = $this->input->get('filter_id');
+		$sql = "select id,name from db_clusters where memo!='' and memo is not null and status!='deleted'";
+		if (!empty($username)) {
+			$sql .= " and nick_name like '%$username%'";
+		}
+		if ($filterId) {
+			$sql .= " and id <> '{$filterId}'";
+		}
+		$sql .= ' order by id desc';
+		$this->load->model('Cluster_model');
+		$res = $this->Cluster_model->getList($sql);
+		if ($res === false) {
+			$res = [];
+		}
+		echo json_encode([
+			'code' => 200,
+			'list' => $res,
+			'message' => 'success',
+		]);
 	}
 
 	/**
--- KunlunMonitor/application/helpers/function.php
+++ KunlunMonitor/application/helpers/function.php
@@ -25,12 +25,25 @@ if (!function_exists('rewrite_config')) {
 if (!function_exists('get_pg_con')) {
 	function get_pg_con($host, $port, $user, $pwd, $db)
 	{
-		$db = pg_connect("host={$host} port={$port} dbname={$db} user={$user} password={$pwd}");
+		$db = pg_connect("host={$host} port={$port} dbname={$db} user={$user} password={$pwd} connect_timeout=5");
 		if (!$db) {
 			throw new ApiException("pg 连接失败:{$host}");
 		}
 
 		return $db;
+	}
+}
+
+if (!function_exists('pg_find')) {
+	function pg_find($con, $sql)
+	{
+		$res = pg_query($con, $sql);
+		if ($res === false) {
+			$err = pg_last_error($con);
+			throw new ApiException('PG 查询失败:' . $err);
+		}
+
+		return pg_fetch_all($res);
 	}
 }
 
